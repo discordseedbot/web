7f208e711db52c11f366a9b2241492789bd052fc
Jyles Coad-Ward <26735262+jylescoad-ward@users.noreply.github.com>
2019-09-29 22:17:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -28,16 +28,16 @@ EOF;
   elseif ($page === "invite") {
     header("Location: https://discordapp.com/oauth2/authorize?client_id=423432378640498688&scope=bot&permissions=8");
   }
-  elseif ($page === "github" || $repo === "stable") {
+  elseif ($repo === "stable") {
     header("Location: https://github.com/discordseedbot/stable");
   }
-  elseif ($page === "github" || $repo === "canary") {
+  elseif ($repo === "canary") {
     header("Location: https://github.com/discordseedbot/canary");
   }
-  elseif ($page === "github" || $repo === "web") {
+  elseif ($repo === "web") {
     header("Location: https://github.com/discordseedbot/web");
   }
-  elseif ($page === "github" || $repo === "changelog") {
+  elseif ($repo === "changelog") {
     header("Location: https://github.com/discordseedbot/changelog");
   }
   elseif ($page === "github" && strlen($repo) < 3) {
