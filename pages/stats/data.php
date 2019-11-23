410acf19eff7b8aa35d691522ff724419c1e32b8
jylescoad-ward <jylescoad-ward@DESKTOP.local>
2019-11-23 17:55:58 +0800

    Made the thingy work on everything now (yay?)

--- pages/stats/data.php
+++ pages/stats/data.php
@@ -24,50 +24,37 @@
 
 	<font>Stats get updated by the bot every miniute, you need to refresh the page yourself, fatass.</font>
 
-	<div class="statsWrapper">
-		<table>
-				<table class="one">
-					<tr>
-						<th>User Count</td>
-						<td><?php echo $userCount; ?></td>
-					</tr>
-					<tr>
-						<th>Guild Count</th>
-						<td><?php echo $guildCount; ?></td>
-					</tr>
-					<tr>
-						<th>Channel Count</th>
-						<td><?php echo $channelCount; ?></td>
-					</tr>
-				</table>
-				<table class="two">
-					<tr>
-						<th>Bot Version</th>
-						<td><?php echo $botVersion; ?></td>
-					</tr>
-					<tr>
-						<th>Bot Build</th>
-						<td><?php echo $botBuild; ?></td>
-					</tr>
-					<tr>
-						<th>Bot Build Date</th>
-						<td><?php echo $botBuildDate; ?></td>
-					</tr>
-				</table>
-				<table class="three">
-					<tr>
-						<th>API Version</th>
-						<td><?php echo $apiVersion; ?></td>
-					</tr>
-					<tr>
-						<th>Bot Branch</th>
-						<td><?php echo $botBranch; ?></td>
-					</tr>
-					<tr>
-						<th>Bot License</th>
-						<td><?php echo $botLicense; ?></td>
-					</tr>
-				</table>
-		</table>
-	</div>
+	<br><br>
+	<table>
+		<tr>
+			<th>User Count</th>
+			<td><?php echo $userCount;?></td>
+			<!-- -->
+			<th>Bot Version</th>
+			<td><?php echo $botVersion;?></td>
+			<!-- -->
+			<th>API Version</th>
+			<td><?php echo $apiVersion;?></td>
+		</tr>
+		<tr>
+			<th>Guild Count</th>
+			<td><?php echo $guildCount;?></td>
+			<!-- -->
+			<th>Bot Build</th>
+			<td><?php echo $botBuild;?></td>
+			<!-- -->
+			<th>Bot Branch</th>
+			<td><?php echo $botBranch;?></td>
+		</tr>
+		<tr>
+			<th>Channel Count</th>
+			<td><?php echo $channelCount;?></td>
+			<!-- -->
+			<th>Bot Build Date</th>
+			<td><?php echo $botBuildDate;?></td>
+			<!-- -->
+			<th>Bot License</th>
+			<td><?php echo $botLicense;?></td>
+		</tr>
+	</table>
 </body>
