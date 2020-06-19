262d40a18e832d87c4c3898eedcfe5f4f542327c
Jyles Coad-Ward <jyles@dariox.club>
2020-06-19 17:38:34 +0800

    added shorter links for plebs

--- inv.php
+++ inv.php
@@ -2,4 +2,15 @@
 	$id = $_GET['a'];
 	$perm = $_GET['b'];
 
+	if(ISSET($_GET['br'])) {
+		switch($_GET['br']) {
+			case "stable":
+				$id = 423432378640498688;
+				break;
+			case "canary":
+				$id = 603130851303096350;
+				break;
+		}
+	}
+
 	header("Location: https://discordapp.com/oauth2/authorize?client_id=".$id."&scope=bot&permissions=".$perm);
